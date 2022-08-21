f87665616e1a09495b7e798349c18c4fbece643d
Mohsen Zahmatkesh <msn.zmt81@gmail.com>
2022-08-21 11:53:26 +0430

    rest framework configed

--- DownloadMovie_BackEnd/DownloadMovie_BackEnd/settings.py
+++ DownloadMovie_BackEnd/DownloadMovie_BackEnd/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'backend.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
